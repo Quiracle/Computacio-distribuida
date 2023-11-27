c0c12a20afb1b37bfc0c2e7ffe72e110408f6dab
Laura Haro Escoi <lauraha.escoi@gmail.com>
2023-11-27 18:31:06 +0100

    ja no peta el influx

--- microservices/save_microservice/save.py
+++ microservices/save_microservice/save.py
@@ -1,25 +1,57 @@
 from kafka import KafkaConsumer
+from influxdb_client import InfluxDBClient, WriteOptions  # Cambio en la importación
 import random
 import logging
 import json
 
-broker_kafka = 'kafka:9092'  # Coloca la dirección de tus brokers Kafka
+# Variables de la base de datos (ajusta según tu configuración)
+db_hostname = 'influxdb'
+db_port = 8086
+db_token = 's2d3rf67ren42i0gg666er9'
+db_org = 'computacio'
+
+broker_kafka = 'kafka:9092'
 topic_kafka = ["sensors-raw", "sensors-clean"]
 CLIENT_ID_KAFKA = f'python-kafka-{random.randint(0, 1000)}-1'
 
 def connect_kafka_consumer():
     return KafkaConsumer(
-        bootstrap_servers=['kafka:9092'],
+        bootstrap_servers=[broker_kafka],
         auto_offset_reset='latest',
         enable_auto_commit=True,
         group_id='my-group2',
-        api_version=(0,11,5),
+        api_version=(0, 11, 5),
         value_deserializer=lambda x: json.loads(x.decode('utf-8'))
     )
 
+def prepareDbClient():
+    client = InfluxDBClient(
+        url=f"http://{db_hostname}:{db_port}",
+        token=db_token,
+        org=db_org,
+    )
+    write_options = WriteOptions()
+    write_options.batch_size = 500
+    write_options.flush_interval = 10_000
+    write_options.flush_jitter = 1_000
 
-# this function subscrives to the defined argument topics ans defines on message function
-def subscribe(consumer_kafka):
+    twrite_api = client.write_api(write_options=write_options)
+    return client, twrite_api
+
+
+
+def save_to_influxdb(influx_client, twrite_api, data):
+    json_body = [
+        {
+            "measurement": "your_measurement_name",
+            "fields": data,
+        }
+    ]
+
+    # Utilizamos el método write del write_api
+    twrite_api.write(bucket=db_org, record=json_body)
+
+def subscribe(consumer_kafka, influx_client, twrite_api):
     consumer_kafka.subscribe(topic_kafka)
     logging.info("Subscribed to topic")
 
@@ -28,18 +60,19 @@ def subscribe(consumer_kafka):
 
         if msg is None:
             logging.info("No message received by consumer")
-        elif msg.error() is not None:
-            logging.error(f"Received error from consumer {msg.error()}")
+        elif 'error' in msg:
+            logging.error(f"Received error from consumer {msg['error']}")
         else:
-            logging.info(f"Received message from consumer {msg.value().decode('utf-8')}")
+            data = msg.get('value', {})
+            logging.info(f"Received message from consumer {data}")
+            save_to_influxdb(influx_client, twrite_api, data)
 
 def run():
     logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', level=logging.INFO)
 
     consumer_kafka = connect_kafka_consumer()
-    consumer_kafka.subscribe(topic_kafka)
-    for message in consumer_kafka:
-        message = message.value
-        logging.info(f"{message} is being processed")
+    influx_client, twrite_api = prepareDbClient()
+    subscribe(consumer_kafka, influx_client, twrite_api)
+
 if __name__ == '__main__':
     run()
